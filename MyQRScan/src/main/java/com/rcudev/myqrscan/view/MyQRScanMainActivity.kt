d34113fd85a1c4376e3120e5d14d535011f3aa82
Rcorvoun <raul.corvo.una@everis.com>
2021-09-22 22:40:33 +0200

    UPDATE: add banner admob

--- MyQRScan/src/main/java/com/rcudev/myqrscan/view/MyQRScanMainActivity.kt
+++ MyQRScan/src/main/java/com/rcudev/myqrscan/view/MyQRScanMainActivity.kt
@@ -2,15 +2,22 @@ package com.rcudev.myqrscan.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View.GONE
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import com.google.android.gms.ads.*
 import com.google.zxing.integration.android.IntentIntegrator
+import com.rcudev.myqrscan.R
 import com.rcudev.myqrscan.data.local.model.QRItem
 import com.rcudev.myqrscan.view.qrList.RecentScanViewModel
 import com.rcudev.myqrscan.view.qrList.components.RecentScanScreen
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MyQRScanMainActivity : ComponentActivity() {
@@ -19,8 +26,25 @@ class MyQRScanMainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            RecentScanScreen(this, viewModel)
+        setContentView(R.layout.activity_my_qr_scan_main)
+        findViewById<ComposeView>(R.id.myqrscan_compose_container).setContent {
+            Surface(modifier = Modifier.fillMaxSize()) {
+                RecentScanScreen(this, viewModel)
+            }
+        }
+
+        initAdMob()
+    }
+
+    private fun initAdMob() {
+        MobileAds.initialize(this) { }
+        val adRequest = AdRequest.Builder().build()
+        val bannerAdView: AdView = findViewById(R.id.myqrscan_admob_banner)
+        bannerAdView.loadAd(adRequest)
+        bannerAdView.adListener = object : AdListener() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                bannerAdView.visibility = GONE
+            }
         }
     }
 
