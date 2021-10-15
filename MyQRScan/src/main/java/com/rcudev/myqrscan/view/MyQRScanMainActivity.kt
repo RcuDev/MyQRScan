0e01fdb0a87d8e50203e1b0fd2e8fca6f7832aa0
Rcorvoun <raul.corvo.una@everis.com>
2021-10-15 20:55:47 +0200

    FIX: ads

--- MyQRScan/src/main/java/com/rcudev/myqrscan/view/MyQRScanMainActivity.kt
+++ MyQRScan/src/main/java/com/rcudev/myqrscan/view/MyQRScanMainActivity.kt
@@ -2,15 +2,16 @@ package com.rcudev.myqrscan.view
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
-import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
+import com.google.android.gms.ads.admanager.AdManagerAdRequest
 import com.google.zxing.integration.android.IntentIntegrator
 import com.rcudev.myqrscan.R
 import com.rcudev.myqrscan.data.local.model.QRItem
@@ -19,9 +20,11 @@ import com.rcudev.myqrscan.view.qrList.components.QRListScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MyQRScanMainActivity : ComponentActivity() {
+class MyQRScanMainActivity : AppCompatActivity() {
 
     private val viewModel: QRListViewModel by viewModels()
+
+    private lateinit var bannerAdView: AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,10 +38,31 @@ class MyQRScanMainActivity : ComponentActivity() {
         initAdMob()
     }
 
+    public override fun onPause() {
+        bannerAdView.pause()
+        super.onPause()
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        bannerAdView.resume()
+    }
+
+    public override fun onDestroy() {
+        bannerAdView.destroy()
+        super.onDestroy()
+    }
+
     private fun initAdMob() {
+        bannerAdView = findViewById(R.id.myqrscan_admob_banner)
         MobileAds.initialize(this) { }
-        val adRequest = AdRequest.Builder().build()
-        val bannerAdView: AdView = findViewById(R.id.myqrscan_admob_banner)
+        MobileAds.setRequestConfiguration(
+            RequestConfiguration.Builder()
+                .setTagForChildDirectedTreatment(RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE)
+                .setMaxAdContentRating(RequestConfiguration.MAX_AD_CONTENT_RATING_G)
+                .build()
+        )
+        val adRequest = AdManagerAdRequest.Builder().build()
         bannerAdView.loadAd(adRequest)
     }
 
